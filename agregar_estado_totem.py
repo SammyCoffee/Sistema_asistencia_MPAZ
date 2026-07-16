3121b245894eca15b55b5dd8183dbd6f2c3f178c
SammyCoffee <tap.tapia@duocuc.cl>
2026-07-16 13:58:01 -0400

    Agregar columna 'estado' a la tabla 'totems'; verificar existencia antes de la adición

---
+++ agregar_estado_totem.py
@@ -0,0 +1,33 @@
+from base_datos import obtener_conexion
+
+
+
+conexion = obtener_conexion()
+cursor = conexion.cursor()
+
+try:
+
+    cursor.execute("PRAGMA table_info(totems)")
+
+    columnas = cursor.fetchall()
+    nombres_columnas = []
+
+    for columna in columnas:
+        nombres_columnas.append(columna[1])
+
+    if "estado" in nombres_columnas:
+        print("La columna estado ya existe")
+
+    else: 
+        cursor.execute(
+            """
+            ALTER TABLE totems
+            ADD COLUMN estado TEXT NOT NULL DFAULT 'activo'    
+            """
+        )    
+
+        conexion.commit()
+
+        print("Columna estado agregado correctamente")
+finally:
+    conexion.close()   
