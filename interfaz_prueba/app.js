c109d0dc4cb1031d34c545119f6a985ac4b35c90
SammyCoffee <tap.tapia@duocuc.cl>
2026-08-11 11:40:00 -0400

    Agrega busqueda de estudiante por RUT

--- interfaz_prueba/app.js
+++ interfaz_prueba/app.js
@@ -2,6 +2,8 @@ const estudianteDemo = {
 
     nombre: "Estudiante Demo",
 
+    rut: "RUT-DEMO-01",
+
     curso: "2A",
 
     uid: "B1C2D3E4"
@@ -31,6 +33,10 @@ botonBuscar.addEventListener("click", function () {
     if (textoBuscado.toLowerCase() === estudianteDemo.nombre.toLowerCase()) {
 
         alert("Estudiante encontrado: " + estudianteDemo.nombre);
+    
+    } else if (textoBuscado.toLowerCase() === estudianteDemo.rut.toLowerCase()) {
+        alert("Estudiante encontrado: " + estudianteDemo.nombre);
+       
 
     } else if (textoBuscado.toLowerCase() === estudianteDemo.curso.toLowerCase()) {
 
